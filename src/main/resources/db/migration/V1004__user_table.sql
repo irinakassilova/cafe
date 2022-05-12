792b869a319963fef7221ef1f23f2069d9193550
Ирина Касилова <irinakasilova1@gmail.com>
2022-05-12 23:06:01 +0600

    added db migration files

---
+++ src/main/resources/db/migration/V1004__user_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+                       id serial primary key  NOT NULL,
+                       email varchar(128) NOT NULL,
+                       password varchar(128) NOT NULL,
+                       enabled boolean NOT NULL default true,
+                       role varchar(128),
+                       UNIQUE (email)
+);
+
+-- alter table if exists user_role
+-- add constraint user_role_user_fk
+-- foreign key (user_id) references users;
+--
+insert into users(email, password, enabled, role)
+values ('admin@mail.com', '$2a$12$Vb8RRJHpUhf4nLeeiIJYQet4.//zUK2ZLFeumFrO1Q6oWpuuoSYha', true, 'ADMIN'),
+       ('manager@mail.com','$2a$12$Vb8RRJHpUhf4nLeeiIJYQet4.//zUK2ZLFeumFrO1Q6oWpuuoSYha', true, 'MANAGER');
+
